c23d5e43b831b49ac70665ecf102d1dc5fb9df27
Aseempaul007 <aseempal007@gmail.com>
2022-11-02 11:30:48 +0530

    Second Commit!

--- app/src/main/java/com/example/androidlifecyle/MainActivity.java
+++ app/src/main/java/com/example/androidlifecyle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,41 +14,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.v("TAG","Oncreate Called");
+        Toast.makeText(MainActivity.this,"Oncreate Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.v("TAG","onStart Called");
+        Toast.makeText(MainActivity.this,"onStart Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.v("TAG","onResume Called");
+        Toast.makeText(MainActivity.this,"onResume Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.v("TAG","onPause Called");
+        Toast.makeText(MainActivity.this,"onPause Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.v("TAG","onStop Called");
+        Toast.makeText(MainActivity.this,"onStop Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.v("TAG","onDestroy Called");
+        Toast.makeText(MainActivity.this,"onDestroy Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.v("TAG","onRestart Called");
+        Toast.makeText(MainActivity.this,"onRestart Called",Toast.LENGTH_LONG).show();
     }
 }
